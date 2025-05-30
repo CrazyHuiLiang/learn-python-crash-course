f0fd3f8bc5ddaf5e709656f75f0573fa473bf2a6
huiliang.zhang <huiliang.zhang@jinshuschool.com>
2025-05-30 13:06:03 +0800

    while and input

---
+++ ch07 while input/7.1 input.py
@@ -0,0 +1,7 @@
+
+message = input("请输入你的名字：")
+print("hello " + message)
+
+age = int(input("请输入你的年龄："))
+print("age = " + str(age))
+
---
+++ ch07 while input/7.3 while list.py
@@ -0,0 +1,17 @@
+
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+
+# 列表为空时循环结束
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+    print("Verifying user: " + current_user.title())
+    confirmed_users.append(current_user)
+
+# 删除为特定值的列表元素
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+while 'cat' in pets:
+    pets.remove('cat')
+print(pets)
+
